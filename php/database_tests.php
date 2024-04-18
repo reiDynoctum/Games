2f58c0a5ddebc3348048adcd8368e45275ba122f
Radek Rybár <reidynoctum@gmail.com>
2024-04-18 16:59:51 +0200

    Database vytvoreni

--- php/database_tests.php
+++ php/database_tests.php
@@ -12,6 +12,7 @@ $connection = new PDO(
     )
 );
 
+
 /* 
 Role:
 0 - běžný uživatel
@@ -19,10 +20,42 @@ Role:
 2 - superadmin (může měnit vše)
 */ 
 
-$statement = $connection->prepare('INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, :role)');
+/*$statement = $connection->prepare('INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, :role)');
 $statement->execute([
     'name' => 'reidy',
     'email' => 'reidynoctum@gmail.com',
     'password' => password_hash('123456789', PASSWORD_BCRYPT),
     'role' => 2
-]);
+]);*/
+
+function createUser(): void{
+    global $connection;
+
+
+    $statement = $connection->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+    $statement->execute([
+        'name' => 'noctum',
+        'email' => 'noctumpw@gmail.com',
+        'password' => password_hash('123456789', PASSWORD_BCRYPT),
+    ]);
+
+}
+
+function deleteUser(): void{
+    global $connection;
+
+    $statement = $connection->prepare('DELETE FROM users WHERE id=:id');
+    $statement->execute(['id' => 23]);
+}
+
+function createCategory(string $name, string $slug): void {
+    global $connection;
+
+    $statement = $connection->prepare('INSERT INTO categories (name, slug) VALUES (:name, :slug)');
+    $statement->execute([
+        'name' => $name,
+        'slug' => $slug
+    ]);
+}
+
+createCategory('Návody', 'navody');
